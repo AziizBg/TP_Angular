0b5b62fbc6e8b713b496752e4f3fd1a9f8c13495
= <mohamedaziz.benghorbel@insat.ucar.tn>
2024-11-08 19:22:33 +0100

    exercice cv details

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -1,17 +1,19 @@
-import { Component } from "@angular/core";
-import { Cv } from "../model/cv";
-import { LoggerService } from "../../services/logger.service";
-import { ToastrService } from "ngx-toastr";
-import { CvService } from "../services/cv.service";
+import { Component } from '@angular/core';
+import { Cv } from '../model/cv';
+import { LoggerService } from '../../services/logger.service';
+import { ToastrService } from 'ngx-toastr';
+import { CvService } from '../services/cv.service';
+import { catchError, Observable, of } from 'rxjs';
 @Component({
-  selector: "app-cv",
-  templateUrl: "./cv.component.html",
-  styleUrls: ["./cv.component.css"],
+  selector: 'app-cv',
+  templateUrl: './cv.component.html',
+  styleUrls: ['./cv.component.css'],
 })
 export class CvComponent {
   cvs: Cv[] = [];
+  cvs$: Observable<Cv[]>;
   selectedCv: Cv | null = null;
-  /*   selectedCv: Cv | null = null; */
+  selectedCv$: Observable<Cv | null> ;
   date = new Date();
 
   constructor(
@@ -19,19 +21,30 @@ export class CvComponent {
     private toastr: ToastrService,
     private cvService: CvService
   ) {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
+    this.cvs$ = this.cvService.getCvs().pipe(
+      catchError((error) => {
         this.toastr.error(`
           Attention!! Les données sont fictives, problème avec le serveur.
           Veuillez contacter l'admin.`);
-      },
-    });
-    this.logger.logger("je suis le cvComponent");
-    this.toastr.info("Bienvenu dans notre CvTech");
-    this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+        return of(this.cvService.getFakeCvs());
+      })
+    );
+
+    // this.cvService.getCvs().subscribe({
+    //   next: (cvs) => {
+    //     this.cvs = cvs;
+    //   },
+    //   error: () => {
+    // this.cvs = this.cvService.getFakeCvs();
+    // this.toastr.error(`
+    //   Attention!! Les données sont fictives, problème avec le serveur.
+    //   Veuillez contacter l'admin.`);
+    //   },
+    // });
+    
+    this.logger.logger('je suis le cvComponent');
+    this.toastr.info('Bienvenu dans notre CvTech');
+    // this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+    this.selectedCv$ = this.cvService.selectCv$;
   }
 }
--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { APP_ROUTES } from '../../../config/routes.config';
 import { AuthService } from '../../auth/services/auth.service';
+import { catchError, Observable, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-details-cv',
@@ -13,6 +14,10 @@ import { AuthService } from '../../auth/services/auth.service';
 })
 export class DetailsCvComponent implements OnInit {
   cv: Cv | null = null;
+
+  // observable
+  cv$!: Observable<Cv | null> | null;
+
   constructor(
     private cvService: CvService,
     private router: Router,
@@ -22,27 +27,64 @@ export class DetailsCvComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    const id = this.activatedRoute.snapshot.params['id'];
-    this.cvService.getCvById(+id).subscribe({
-        next: (cv) => {
-          this.cv = cv;
-        },
-        error: (e) => {
+    // observable: params
+    this.cv$ = this.activatedRoute.params.pipe(
+      switchMap((params) =>
+        this.cvService.getCvById(+params['id']).pipe(
+          catchError((e) => {
+            this.router.navigate([APP_ROUTES.cv]);
+            return of(null);
+          })
+        )
+      )
+    );
+
+    // snapshot
+    // const id = +this.activatedRoute.snapshot.params['id'];
+    // this.cv$ = this.cvService.getCvById(id).pipe(
+    //   catchError(() => {
+    //     this.router.navigate([APP_ROUTES.cv]);
+    //     return of(null);
+    //   })
+    // );
+
+    // this.cvService.getCvById(+id).subscribe({
+    //     next: (cv) => {
+    //       this.cv = cv;
+    //     },
+    //     error: (e) => {
+    //       this.router.navigate([APP_ROUTES.cv]);
+    //     },
+    //   });
+  }
+  deleteCv(cv: Cv) {
+    this.cvService
+      .deleteCvById(cv.id)
+      .pipe(
+        catchError(() => {
+          this.toastr.error(
+            `Problème avec le serveur veuillez contacter l'admin`
+          );
+          return of(null);
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.toastr.success(`${cv.name} supprimé avec succès`);
           this.router.navigate([APP_ROUTES.cv]);
         },
       });
-  }
-  deleteCv(cv: Cv) {
-    this.cvService.deleteCvById(cv.id).subscribe({
-      next: () => {
-        this.toastr.success(`${cv.name} supprimé avec succès`);
-        this.router.navigate([APP_ROUTES.cv]);
-      },
-      error: () => {
-        this.toastr.error(
-          `Problème avec le serveur veuillez contacter l'admin`
-        );
-      },
-    });
+
+    // this.cvService.deleteCvById(cv.id).subscribe({
+    //   next: () => {
+    //     this.toastr.success(`${cv.name} supprimé avec succès`);
+    //     this.router.navigate([APP_ROUTES.cv]);
+    //   },
+    //   error: () => {
+    //     this.toastr.error(
+    //       `Problème avec le serveur veuillez contacter l'admin`
+    //     );
+    //   },
+    // });
   }
 }
