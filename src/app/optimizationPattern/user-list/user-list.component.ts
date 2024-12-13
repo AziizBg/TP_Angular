bf3a8dc1e85948b2778fa8e9d35d5f2469922d38
AzizBenGhorbel <mohamedaziz.benghorbel@insat.ucar.tn>
2024-12-13 22:37:10 +0100

    TP3 q5

--- src/app/optimizationPattern/user-list/user-list.component.ts
+++ src/app/optimizationPattern/user-list/user-list.component.ts
@@ -1,21 +1,28 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {User} from "../users.service";
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { User } from '../users.service';
+import Memoize from 'memo-decorator';
 import { FormsModule } from '@angular/forms';
 
-
 export const fibonnaci = (n: number): number => {
-  if (n==1 || n==0) {
+  if (n == 1 || n == 0) {
     return 1;
   }
-  return fibonnaci(n-1) + fibonnaci(n-2);
-}
+  return fibonnaci(n - 1) + fibonnaci(n - 2);
+};
 
 @Component({
-    selector: 'app-user-list',
-    templateUrl: './user-list.component.html',
-    styleUrls: ['./user-list.component.css'],
-    standalone: true,
-    imports: [FormsModule]
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush, // This component will only be updated when the input changes
+  standalone: true,
+  imports: [FormsModule],
 })
 export class UserListComponent {
   @Input() usersCluster: string = '';
@@ -26,9 +33,10 @@ export class UserListComponent {
     this.add.emit(this.userFullName);
     this.userFullName = '';
   }
+  @Memoize() // Memoize the result of the function to avoid recalculating the same value
   fibo(n: number): number {
     const fib = fibonnaci(n);
-    console.log({n, fib});
+    console.log({ n, fib });
 
     return fib;
   }
